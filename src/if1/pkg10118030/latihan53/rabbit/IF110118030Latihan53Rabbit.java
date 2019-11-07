d74316fbbe8c1bc901289e5b869543a556c8254c
arisulistio37 <arisulistio37@gmail.com>
2019-11-07 19:49:27 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan53/rabbit/IF110118030Latihan53Rabbit.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan53.rabbit;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ * Deskripsi program : program ini berisi tentang rabbit
+ */
+public class IF110118030Latihan53Rabbit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Rabbit rabbit = new Rabbit("Peter",false, "grass", 4, "grey" );
+        System.out.println("Hello, His name is "+rabbit.getName());
+        System.out.println(rabbit.getName()+" is Vegetarian? "+rabbit.isVegetarian());
+        System.out.println(rabbit.getName()+" eats "+rabbit.getEats());
+        System.out.println(rabbit.getName()+" has "+rabbit.getNoOfLegs()+" legs.");
+        System.out.println(rabbit.getName()+" color is "+rabbit.getColor());
+    }
+    
+}
